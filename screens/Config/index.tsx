628b586c4e9a9b3ac7ea074df1f3ec5da6cc1523
PabloGLeiteDeSouza <pablo.guilherme.leite.de.souza@gmail.com>
2024-09-11 18:06:57 -0300

    "Added tabBarIcon to ConfigurationsScreen and updated ConfigurationsScreen with new UI components and layout"

--- screens/Config/index.tsx
+++ screens/Config/index.tsx
@@ -1,10 +1,23 @@
-import { Box, ScrollView, Text } from '@gluestack-ui/themed';
+import { Box, Button, ButtonText, ScrollView, Text, VStack } from '@gluestack-ui/themed';
 
 const ConfigurationsScreen: React.FC = () => {
+
   return (
     <ScrollView>
       <Box h="$full" w="$full">
-        <Text>Configurations Screen</Text>
+        <VStack>
+            <Box>
+                <Text size="xl">Configurations</Text>
+            </Box>
+            <Box>
+                <Text size="lg">Exportar dados da aplicação:</Text>
+                <Button>
+                    <ButtonText>
+                        Exportar
+                    </ButtonText>
+                </Button>
+            </Box>
+        </VStack>
       </Box>
     </ScrollView>
   );
