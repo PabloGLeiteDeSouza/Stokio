1c809a0a82cd7d2912e3846693a8792db1ff20d2
PabloGLeiteDeSouza <pablo.guilherme.leite.de.souza@gmail.com>
2024-10-02 00:52:53 -0300

    Adiction new files updated

--- src/app/screens/config/index.tsx
+++ src/app/screens/config/index.tsx
@@ -1,5 +1,22 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+const Stack = createNativeStackNavigator();
+
 const ConfigScreens: React.FC = () => {
-  return <></>;
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="" />
+            <Stack.Screen />
+            <Stack.Screen />
+            <Stack.Screen />
+            <Stack.Screen />
+        </Stack.Navigator>
+    );
 };
+
+const ConfigScreen: React.FC = () => {
+    return <></>;
+}
 
 export default ConfigScreens;
